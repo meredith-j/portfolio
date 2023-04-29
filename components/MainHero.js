9b928d38f4953440702ea61a5c1a01868668bbfb
Meredith Jonatan <mjonatan@me.com>
2023-04-29 00:47:45 -0400

    confirm styles, add max-width, hero is responsive

--- components/MainHero.js
+++ components/MainHero.js
@@ -3,9 +3,9 @@ import styles from '../styles/MainHero.module.scss'
 const MainHero = () => {
     return (
     <div className={styles.hero}>
-        <h1 className={styles.hero_header}>hello,</h1>
-        <h1 className={styles.hero_header}>i'm meredith</h1>
-        <p className={styles.hero_subheader}>i'm a full stack software engineer</p>
+        <h1 className={`${styles.hero_header} ${styles.hero_gradient}`}>hello,</h1>
+        <h1 className={`${styles.hero_header} ${styles.hero_gradient}`}>i'm meredith</h1>
+        <p className={`${styles.hero_subheader} ${styles.hero_gradient}`}>i'm a full stack software engineer</p>
     </div>
     )
 }
