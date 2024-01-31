e75f853413db144f48f741bd41e7b2e3bcfda368
Meredith Jonatan <mjonatan@me.com>
2024-01-30 20:04:22 -0500

    fixed prod build, updated styles where needed

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ import styles from '../styles/Footer.module.scss';
 const Footer = () => {
     return (
     <div className={styles.footer}>
-        <p className={styles.text}>Meredith Jonatan © 2023</p>
-        <p className={styles.text}>Hire me! <a href='https://www.linkedin.com/in/meredithjonatan/'className={styles.link}>Learn more</a> about my career.</p>
+        <p className={styles.text}>Hire me! <a href='https://www.linkedin.com/in/meredithjonatan/'className={styles.link}>Learn more</a> about my career. See this code for <a href='https://www.linkedin.com/in/meredithjonatan/'className={styles.link}>this website</a>.</p>
+        <p className={styles.text}>Meredith Jonatan © 2024</p>
     </div>
     )
 }
