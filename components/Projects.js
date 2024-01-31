64dad4e1e0f4c6f5b6f36a8f3099fbf5664e3109
Meredith Jonatan <mjonatan@me.com>
2024-01-30 20:04:22 -0500

    fixed prod build, updated styles where needed

--- components/Projects.js
+++ components/Projects.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import styles from '../styles/Projects.module.scss'
 import githubLogo from '../public/icons/githubprojects.svg'
+import websiteUrl from '../public/icons/websiteurl.svg'
 import Image from 'next/image'
 
 const Projects = ({ allProjectsData }) => {
@@ -17,23 +18,32 @@ const Projects = ({ allProjectsData }) => {
               alt={`Screenshot of ${title}`}
               />
           <div className={styles.projects_links}>
-            <Link
-              href={github}
-              className={`${styles.projects_githublink} ${styles.projects_link}`}>
-              <Image
-                priority
-                src={githubLogo}
-                alt="Github link to code"
-                className={styles.projects_github}
-                />
-              <p className={styles.projects_githubtext}>See the code!</p>
-            </Link>
-            {/* note: none of these are live except the portfolio */}
-            {/* <Link
-              href={liveUrl}
-              className={styles.projects_link}>
-              <p className={styles.projects_live}>Visit the Site!</p>
-            </Link> */}
+          { github === "" ? "" : (
+                  <Link
+                  href={github}
+                  className={`${github} === "" ? ${styles.projects_link} : ${styles.projects_link}`}>
+                  <Image
+                    priority
+                    src={githubLogo}
+                    alt="Github link to code"
+                    className={styles.projects_link_image}
+                    />
+                  <p className={styles.projects_link_text}>Code Base</p>
+                </Link>
+                )}
+          { liveUrl === "" ? "" : (
+                  <Link
+                  href={liveUrl}
+                  className={styles.projects_link}>
+                    <Image
+                    priority
+                    src={websiteUrl}
+                    alt={`Link to ${title}`}
+                    className={styles.projects_link_image}
+                    />
+                    <p className={styles.projects_link_text}>Live Site</p>
+                </Link>
+                )}
           </div>
           <div className={styles.projects_info}>
             <p className={styles.projects_description}>{details}</p>
--- projects/aileen-miziolek.md
+++ projects/aileen-miziolek.md
@@ -1,9 +1,10 @@
 ---
-title: "Aileen Miziolek"
-date: "2023-06-01"
-image: "/images/aileen-miziolek.png"
+title: "Client Work: Aileen Miziolek"
+date: "2024-01-29"
+image: "/images/aileen-miziolek-desktop.png"
 techStack: "Wix, EditorX, JavaScript"
 github: ""
-liveUrl: "https://www.aileenmiziolek.com"
+liveUrl: "https://www.aileenmiziolek.com/"
 details: "Aileen Miziolek is an established family business consultant and wanted to take her digital brand to the next level. I worked with Aileen's brand manager to design & build a responsive, accessible website where Aileen can grow her platform."
 ---
+
