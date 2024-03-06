23e6892dafe03c1280b0297e0722aff52c0da246
Meredith Jonatan <mjonatan@me.com>
2024-03-05 23:56:30 -0500

    updated footer links

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.scss';
 const Footer = () => {
     return (
     <div className={styles.footer}>
-        <p className={styles.text}>Hire me! <a href='https://www.linkedin.com/in/meredithjonatan/'className={styles.link}>Learn more</a> about my career. See this code for <a href='https://www.linkedin.com/in/meredithjonatan/'className={styles.link}>this website</a>.</p>
+        <p className={styles.text}>Hire me! <a href='https://www.linkedin.com/in/meredithjonatan/'className={styles.link}>Learn more</a> about my career. See the code for <a href='https://github.com/meredith-j/portfolio'className={styles.link}>this website</a>.</p>
         <p className={styles.text}>Meredith Jonatan © 2024</p>
     </div>
     )
